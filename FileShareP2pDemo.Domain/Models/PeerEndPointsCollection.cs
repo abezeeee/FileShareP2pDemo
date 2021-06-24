a05f9dd2eb199a2f45512a88a93e3751e6ebb9b1
abezeeee <43694235+abezeeee@users.noreply.github.com>
2021-06-24 16:18:30 +0700

    Add files via upload
    upload rest files

---
+++ FileShareP2pDemo.Domain/Models/PeerEndPointsCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.PeerToPeer;
+
+namespace FileShareP2pDemo.Domain.Models
+{
+    public class PeerEndPointsCollection
+    {
+        public PeerEndPointsCollection(PeerName peer, IPEndPointCollection ipEndPoint)
+        {
+            PeerHostName = peer;
+            PeerEndPoints = ipEndPoint; 
+        }
+
+        public PeerName PeerHostName { get; set; }
+        public IPEndPointCollection PeerEndPoints { get; set; }
+
+    }
+}
---
+++ FileShareP2pDemo.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FileShareP2pDemo.Domain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileShareP2pDemo.Domain")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d6259ce-19f8-4e65-937d-5f24cd2d867d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
